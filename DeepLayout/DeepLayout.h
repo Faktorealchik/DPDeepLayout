ab6586aac9e871d369d14944e983b7cd6f9496d0
nesterov <faktorealchik@gmail.com>
2018-09-10 21:20:32 +0300

    Initial Commit

---
+++ DeepLayout/DeepLayout.h
@@ -0,0 +1,19 @@
+//
+//  DeepLayout.h
+//  DeepLayout
+//
+//  Created by Alexandr Nesterov on 9/10/18.
+//  Copyright © 2018 Alexandr Nesterov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeepLayout.
+FOUNDATION_EXPORT double DeepLayoutVersionNumber;
+
+//! Project version string for DeepLayout.
+FOUNDATION_EXPORT const unsigned char DeepLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeepLayout/PublicHeader.h>
+
+
